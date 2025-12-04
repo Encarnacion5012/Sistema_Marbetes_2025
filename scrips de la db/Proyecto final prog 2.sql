d9dc7354f1f4efcb93b1443579fabebe2282ea57
Encarnacion5012 <estainlinv@gmail.com>
2025-12-03 23:34:42 -0400

    Refactor SQL script for database setup and inserts
    Updated SQL script to create and populate database tables for vehicles, marbetes, configurations, and renovations. Removed commented sections and added necessary insert statements.

--- scrips de la db/Proyecto final prog 2.sql
+++ scrips de la db/Proyecto final prog 2.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS db_marbetes;
 USE db_marbetes;
 
-renovacionesmarbetes-- 1. Tabla Vehículos (Requerido: placa, marca, modelo, año, id_propietario, renovado)
+
 CREATE TABLE vehiculos (
     placa VARCHAR(20) PRIMARY KEY,
     marca VARCHAR(50) NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE vehiculos (
     renovado BOOLEAN DEFAULT FALSE NOT NULL
 );
 
--- 2. Tabla Marbetes (Requerido: secuencia, año inicio/fin, vendido)
+
 CREATE TABLE marbetes (
     secuencia INT PRIMARY KEY,
     anio_inicio_renovacion INT NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE marbetes (
     vendido BOOLEAN DEFAULT FALSE NOT NULL
 );
 
--- 3. Tabla Configuraciones (Para los costos y fechas)
+
 CREATE TABLE configuraciones (
     id INT PRIMARY KEY,
     costo_bajo DECIMAL(10,2) NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE configuraciones (
     fecha_fin_renovacion DATETIME NOT NULL
 );
 
--- 4. Tabla Renovaciones (Registro de ventas)
+
 CREATE TABLE renovaciones (
     id INT AUTO_INCREMENT PRIMARY KEY,
     placa VARCHAR(20) NOT NULL,
@@ -52,42 +52,28 @@ SELECT
 FROM renovaciones
 GROUP BY DATE(fecha_renovacion);
 
-renovaciones
 
-USE db_marbetes;
 
--- 1. CONFIGURACIÓN (OBLIGATORIO)
--- Reglas: Autos hasta 2019 pagan 1500, desde 2020 pagan 3000.
+
 INSERT INTO configuraciones (id, costo_bajo, costo_alto, anio_fabricacion_inicia_alto, fecha_inicio_renovacion, fecha_fin_renovacion) 
 VALUES (1, 1500.00, 3000.00, 2020, '2025-01-01 00:00:00', '2026-01-01 23:59:59');
 
--- 2. INVENTARIO DE MARBETES (OBLIGATORIO)
--- Debes tener marbetes disponibles (vendido = 0) para poder vender.
+
 INSERT INTO marbetes (secuencia, anio_inicio_renovacion, anio_fin_renovacion, vendido) VALUES 
 (1001, 2025, 2026, 0),
 (1002, 2025, 2026, 0),
 (1003, 2025, 2026, 0);
 
--- 3. VEHÍCULOS DE PRUEBA
--- Caso A: Auto Viejo (2015) -> Debería pagar $1500
-marbetes
 
--- Caso B: Auto Nuevo (2022) -> Debería pagar $3000
+
+
 INSERT INTO vehiculos (placa, marca, modelo, anio_fabricacion, identificacion_propietario, renovado) 
 VALUES ('X999', 'Tesla', 'Model 3', 2022, '001-9876543-2', 0);
 
--- Caso C: Auto ya Renovado -> Debería dar error
+
 INSERT INTO vehiculos (placa, marca, modelo, anio_fabricacion, identificacion_propietario, renovado) 
 VALUES ('R555', 'Honda', 'Civic', 2018, '001-5555555-5', 1);
 
 ALTER TABLE renovaciones MODIFY id INT AUTO_INCREMENT;
 
 
--- NO ejecutar
-UPDATE vehiculos
-SET renovado = false
-WHERE renovado = TRUE;
-
-UPDATE marbetes
-SET vendido = FALSE
-WHERE vendido = true
